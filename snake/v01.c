729e0944d270f698de9aaca39ec9f5107e0b257f
AndreyPetr <petruhin.andrey1997@mail.ru>
2018-05-28 09:51:17 +0300

    add protatape

--- snake/v01.c
+++ snake/v01.c
@@ -15,6 +15,51 @@
 #define left 97
 #define right 100
 
+typedef struct Snake
+{
+    char symbol;
+    int size;
+    char direction;
+    char prev_direction;
+    int tail_X;
+    int tail_Y;
+    int head_X;
+    int head_Y;
+
+}snake;
+
+
+typedef struct snake_pos
+{
+    int Y[vertical*horizontal];
+    int X[vertical*horizontal];
+
+}snake_pos;
+
+
+void snake_init(snake *snake1);
+void pos_init(snake_pos *pos1);
+void food_init(food *food1);
+void gotoxy(int,int);
+void snake_place(snake *snake1, snake_pos *pos1);
+void snake_move(snake *snake1, snake_pos *pos1, food *food1, int*);
+void move_tail(snake *snake1, snake_pos *pos1);
+void move_head(snake *snake1, snake_pos *pos1);
+void food_print(food *food1);
+int game_over(snake *snake1, snake_pos *pos1);
+void set_borders();
+void print_score(int*);
+int kbhit(void);
+
+typedef struct food
+{
+    int X;
+    int Y;
+    char symbol;
+
+}food;
+
+
 void set_borders();
 void gotoxy(int,int);
 
