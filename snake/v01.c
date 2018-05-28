5d42a7f1ee37d32253650ae6d69a0117ceb9d9d0
AndreyPetr <petruhin.andrey1997@mail.ru>
2018-05-28 09:15:49 +0300

    + ещё одна функция

--- snake/v01.c
+++ snake/v01.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ncurses.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <time.h>
 
 #define vertical 30
 #define horizontal 60
@@ -57,3 +63,29 @@ void gotoxy(int x,int y)
     printf("%c[%d;%df",0x1B,y,x);
 }
 
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
